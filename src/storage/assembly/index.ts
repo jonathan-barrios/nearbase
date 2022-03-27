b323aa8f0c7f7dd28f5f9f8d4abe0e9e0494ad2b
jonathan-barrios <hello@jonathanbarrios.com>
2022-03-26 19:20:36 -0500

    add playGame function

--- src/storage/assembly/index.ts
+++ src/storage/assembly/index.ts
@@ -1,6 +1,6 @@
 import {context, u128, PersistentMap, logging, RNG, Context} from "near-sdk-as"
 
-enum GameState{ Created, InProgress, Complted, NotFound }
+enum GameState{ Created, InProgress, Completed, NotFound }
 
 @nearBindgen
 
@@ -31,4 +31,35 @@ export function createGame(): u32 {
     games.set(game.id, game)
     // logging.log("Game ID is " + game.id.toString())
     return game.id
+}
+
+export function playGame(gameId: u32, guess: u32): string {
+    const game = games.getSome(gameId)
+    if (game != null){
+        if (game.gameState == GameState.Created){
+            game.gameState - GameState.InProgress
+            if (context.sender == game.player1){
+                game.player1Guess = guess
+                let rng = new RNG<u32>(1, 10)
+                const selectedNum = rng.next()
+                game.compGuess = selectedNum;
+
+                if (guess == selectedNum){
+                    game.gameState = GameState.Completed
+                    game.winner = context.sender
+                    games.set(gameId, game)
+                    logging.log("You win!")
+                    return "won"
+                    
+                } else {
+                    game.gameState = GameState.Completed
+                    game.loser = context.sender
+                    games.set(gameId, game)
+                    logging.log("You lost! The computer's number was " + selectedNum.toString())
+                    return "lost"
+                }
+            } return "wrong player"
+        } return "game in progress or over"
+    }
+    return "game not found"
 }
